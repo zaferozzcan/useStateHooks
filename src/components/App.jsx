1c7bb29bf77306f7d5d739dcb58399983b7b23aa
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-05-20 23:11:49 -0400

    Add decrease button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,14 @@ function App() {
     setCount(count + 1);
   }
 
+  function decrease() {
+    setCount(count - 1);
+  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
       <button onClick={increase}>+</button>
     </div>
   );
